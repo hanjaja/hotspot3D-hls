2162b9c319c27b1ef443007ac0c4ecd0adb16e91
Hansaja <hansaja.perera@gmail.com>
2023-11-19 18:00:48 -0800

    initial load, store, compute and tiling implementation (no halos)

--- project/3D.cpp
+++ project/3D.cpp
@@ -6,69 +6,102 @@
 #include <sys/time.h>
 #include <string.h>
 
-#define STR_SIZE (256)
-#define MAX_PD	(3.0e6)
-/* required precision in degrees	*/
-#define PRECISION	0.001
-#define SPEC_HEAT_SI 1.75e6
-#define K_SI 100
-/* capacitance fitting factor	*/
-#define FACTOR_CHIP	0.5
+#define BUFFER_SIZE ... // Maximum buffer size
 
-
-/* chip parameters	*/
-float t_chip = 0.0005;
-float chip_height = 0.016; float chip_width = 0.016; 
 /* ambient temperature, assuming no package at all	*/
 float amb_temp = 80.0;
 
-
-
-void computeTempFPGA(float *pIn, float* tIn, float *tOut, 
-        int nx, int ny, int nz, float Cap, 
-        float Rx, float Ry, float Rz, 
-        float dt, int numiter) 
-{   float ce, cw, cn, cs, ct, cb, cc;
-    float stepDivCap = dt / Cap;
-    ce = cw =stepDivCap/ Rx;
-    cn = cs =stepDivCap/ Ry;
-    ct = cb =stepDivCap/ Rz;
-
-    cc = 1.0 - (2.0*ce + 2.0*cn + 3.0*ct);
-
-    int c,w,e,n,s,b,t;
-    int x,y,z;
-    int i = 0;
-    do{
-        for(z = 0; z < nz; z++)
-            for(y = 0; y < ny; y++)
-                for(x = 0; x < nx; x++)
-                {
-                    c = x + y * nx + z * nx * ny;
-
-                    w = (x == 0) ? c      : c - 1;
-                    e = (x == nx - 1) ? c : c + 1;
-                    n = (y == 0) ? c      : c - nx;
-                    s = (y == ny - 1) ? c : c + nx;
-                    b = (z == 0) ? c      : c - nx * ny;
-                    t = (z == nz - 1) ? c : c + nx * ny;
-
-
-                    tOut[c] = tIn[c]*cc + tIn[n]*cn + tIn[s]*cs + tIn[e]*ce + tIn[w]*cw + tIn[t]*ct + tIn[b]*cb + (dt/Cap) * pIn[c] + ct*amb_temp;
-                }
-        float *temp = tIn;
-        tIn = tOut;
-        tOut = temp; 
-        i++;
+// load in
+void load_in(float *in, float local_in[BUFFER_SIZE], int size) {
+    for (int i = 0; i < size; i++) {
+        //#pragma HLS PIPELINE
+        local_in[i] = in[i];
     }
-    while(i < numiter);
-
 }
 
+// Combined load function
+void load(float *pIn, float *tIn, float local_pIn[BUFFER_SIZE], float local_tIn[BUFFER_SIZE], int size) {
+    #pragma HLS DATAFLOW
+    load_in(pIn, local_pIn, size);
+    load_in(tIn, local_tIn, size);
+}
 
+// Store function
+void store(float* output, float local_buffer[BUFFER_SIZE], int size) {
+    for (int i = 0; i < size; i++) {
+        //#pragma HLS PIPELINE
+        output[i] = local_buffer[i];
+    }
+}
 
+void compute(float local_pIn[BUFFER_SIZE], float local_tIn[BUFFER_SIZE], float local_tOut[BUFFER_SIZE], 
+             int nx, int ny, int nz, float Cap, float Rx, float Ry, float Rz, float dt, int numiter) {
+    float ce, cw, cn, cs, ct, cb, cc;
+    float stepDivCap = dt / Cap;
+    ce = cw = stepDivCap / Rx;
+    cn = cs = stepDivCap / Ry;
+    ct = cb = stepDivCap / Rz;
+    cc = 1.0 - (2.0 * ce + 2.0 * cn + 3.0 * ct);
 
+    for (int iter = 0; iter < numiter; iter++) {
+        for (int z = 0; z < nz; z++) {
+            for (int y = 0; y < ny; y++) {
+                for (int x = 0; x < nx; x++) {
+                    //#pragma HLS PIPELINE
+                    int c = x + y * nx + z * nx * ny;
+                    int w = (x == 0) ? c : c - 1;
+                    int e = (x == nx - 1) ? c : c + 1;
+                    int n = (y == 0) ? c : c - nx;
+                    int s = (y == ny - 1) ? c : c + nx;
+                    int b = (z == 0) ? c : c - nx * ny;
+                    int t = (z == nz - 1) ? c : c + nx * ny;
 
+                    local_tOut[c] = local_tIn[c] * cc + local_tIn[n] * cn + local_tIn[s] * cs + 
+                                    local_tIn[e] * ce + local_tIn[w] * cw + local_tIn[t] * ct + 
+                                    local_tIn[b] * cb + (dt / Cap) * local_pIn[c] + ct * amb_temp;
+                }
+            }
+        }
 
+        // Swap buffers: Copy local_tOut to local_tIn for next iteration
+        for (int i = 0; i < BUFFER_SIZE; i++) {
+            //#pragma HLS PIPELINE
+            local_tIn[i] = local_tOut[i];
+        }
+    }
+}
 
+void hotspot(float *pIn, float* tIn, float *tOut, int nx, int ny, int nz, float Cap, float Rx, float Ry, float Rz, float dt, int numiter) {
+    // TODO:
+    // Implement halo handling
 
+    // Tile dimensions (to be calculated)
+    int tileWidth, tileHeight, tileDepth;
+
+    // Calculate tile dimensions based on dataset size and buffer constraints
+    // Example calculation (adjust as needed based on BUFFER_SIZE and dataset properties)
+    tileWidth = nx;  // Full width
+    tileDepth = nz;  // Full depth
+    tileHeight = BUFFER_SIZE / (tileWidth * tileDepth * sizeof(float)); // Calculate height
+
+    // Ensure tileHeight is within dataset bounds
+    if (tileHeight > ny) {
+        tileHeight = ny;
+    }
+
+    float local_pIn[tileWidth * tileHeight * tileDepth];
+    float local_tIn[tileWidth * tileHeight * tileDepth];
+    float local_tOut[tileWidth * tileHeight * tileDepth];
+
+    // Iterate over tiles in the y-dimension
+    for (int yTile = 0; yTile < ny; yTile += tileHeight) {
+        int actualTileHeight = (yTile + tileHeight <= ny) ? tileHeight : ny - yTile;
+        int tileOffset = yTile * nx * nz;
+        int tileSize = tileWidth * actualTileHeight * tileDepth;
+
+        // Load, compute, store for each tile
+        load(&pIn[tileOffset], &tIn[tileOffset], local_pIn, local_tIn, tileSize);
+        compute(local_pIn, local_tIn, local_tOut, tileWidth, actualTileHeight, tileDepth, Cap, Rx, Ry, Rz, dt, numiter);
+        store(&tOut[tileOffset], local_tOut, tileSize);
+    }
+}
--- project/3D.h
+++ project/3D.h
@@ -1,10 +1,6 @@
 #ifndef _H_3D_H_
 #define _H_3D_H_
 
-
-void computeTempFPGA(float *pIn, float* tIn, float *tOut, 
-        int nx, int ny, int nz, float Cap, 
-        float Rx, float Ry, float Rz, 
-        float dt, int numiter);
+void hotspot(float *pIn, float* tIn, float *tOut, int nx, int ny, int nz, float Cap, float Rx, float Ry, float Rz, float dt, int numiter);
 
 #endif // _H_3D_H_
