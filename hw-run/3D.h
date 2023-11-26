65e7e6ca4b254736ef9a606b08895a9a9c12823d
Danyal Rashid <drashid@sfu.ca>
2023-11-25 20:55:57 -0800

    add hw-run

--- hw-run/3D.h
+++
@@ -1,9 +0,0 @@
-#ifndef _H_3D_H_
-#define _H_3D_H_
-
-void hotspot(float *pIn, float* tIn, float *tOut, 
-        int nx, int ny, int nz, float Cap, 
-        float Rx, float Ry, float Rz, 
-        float dt, int numiter);
-
-#endif // _H_3D_H_
