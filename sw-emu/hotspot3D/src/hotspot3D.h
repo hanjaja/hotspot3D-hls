b916d631e77eac1a5017b10fe84258e93b4478f4
kmokaya <kmokaya@ensc-mmc-13.engineering.sfu.ca>
2023-11-25 18:12:05 -0800

    added sw-emu

---
+++ sw-emu/hotspot3D/src/hotspot3D.h
@@ -0,0 +1,21 @@
+#ifndef _H_3D_H_
+#define _H_3D_H_
+
+#include <ap_int.h>
+#include "mars_wide_bus.h"
+
+#define DWIDTH 512
+#define INTERFACE_WIDTH ap_uint<DWIDTH>
+#define WIDTH_FACTOR (DWIDTH/32)
+
+#define NX 1024  // Fixed x-dimension
+#define NY 1024  // Fixed y-dimension
+#define NZ 8    // Fixed z-dimension
+
+#define NUMITER 2
+
+extern "C"{
+void hotspot3D(INTERFACE_WIDTH pIn[NX*NY*NZ/WIDTH_FACTOR], INTERFACE_WIDTH tIn[NX*NY*NZ/WIDTH_FACTOR], INTERFACE_WIDTH tOut[NX*NY*NZ/WIDTH_FACTOR], 
+             float stepDivCap, float ce, float cw, float cn, float cs, float ct, float cb, float cc);
+}
+#endif // _H_3D_H_
